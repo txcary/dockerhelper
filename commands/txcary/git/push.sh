913502332c0695344d4b86c1e67941ac2021769e
Tan Xiang <tx-cary@163.com>
2018-08-07 03:50:36 +0000

    Add push/pull all commands.

---
+++ commands/txcary/git/push.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+pushAll(){
+	project=$1
+	git -C $project pull
+	git -C $project add -A
+	git -C $project commit
+	git -C $project push
+}
+git config --global user.name "Tan Xiang"
+git config --global user.email tx-cary@163.com
+
+pushAll dockerhelper
+pushAll dockerfiles
+pushAll traceability
